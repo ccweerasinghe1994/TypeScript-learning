f969c601af325b02420700ba1518bfd6fd348035
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-07-07 10:09:08 +0530

    adding debug config

--- 3-The TypeScript Compiler (and its Configuration)/src/app.ts
+++ 3-The TypeScript Compiler (and its Configuration)/src/app.ts
@@ -1,18 +1,16 @@
-
-
 const button = document.querySelector('button');
 
-function add(num:number,num2:number){
-    if(num>num2){
-        return num+num2
-    }
-    return ;
+function add(num: number, num2: number) {
+  if (num > num2) {
+    return num + num2;
+  }
+  return;
 }
 
-function handleClick(message:string){
-    console.log("clicked"+message)
+function handleClick(message: string) {
+  console.log('clicked -------->' + message);
 }
 
 if (button) {
-    button.addEventListener('click', handleClick.bind(null,"abc",12));
+  button.addEventListener('click', handleClick.bind(null, 'abc', 12));
 }
