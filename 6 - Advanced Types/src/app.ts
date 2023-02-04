68228fcc53a1022d8c4e2846f01eca3fa5a9a71b
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-02-04 15:42:30 +0530

    interfaces

--- 6 - Advanced Types/src/app.ts
+++ 6 - Advanced Types/src/app.ts
@@ -49,3 +49,85 @@ printEmployeeInformation({
   privilages: ["SERVER"],
   startDate: new Date(),
 });
+
+class Car {
+  drive() {
+    console.log("car is driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("truck is driving");
+  }
+
+  loading() {
+    console.log("loading ...");
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loading();
+  }
+}
+
+const car1 = new Car();
+const truck = new Truck();
+
+useVehicle(car1);
+useVehicle(truck);
+
+// -------------------
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function getAnimalSpeed(animal: Animal) {
+  let speed: number;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+    default:
+      speed = -1;
+      break;
+  }
+  if (speed > 0) {
+    console.log("animal Speed -> 👽🏠🧪 ->" + speed);
+  }
+}
+
+getAnimalSpeed({
+  type: "bird",
+  flyingSpeed: 30,
+});
+
+// const htmlElement = document.getElementById("user-input") as HTMLInputElement;
+// const htmlElement = <HTMLInputElement>document.getElementById("user-input");
+const htmlElement = document.getElementById("user-input");
+
+(htmlElement as HTMLInputElement).value = "Hi There";
+
+interface ErrorContainer {
+  [id: string]: string;
+}
+
+const error: ErrorContainer = {
+  email: "not a valid email",
+  username: "invalid User name",
+};
